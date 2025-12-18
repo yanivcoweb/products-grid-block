cd600d1a600aa1845ecf6054036b7c3f9aa075aa
yanivcoweb <119358521+yanivcoweb@users.noreply.github.com>
2025-12-18 12:06:34 +0200

    style and save.js changes

--- src/save.js
+++ src/save.js
@@ -12,7 +12,7 @@ export default function save({ attributes }) {
 				}}
 			>
 				{products.map((product) => (
-					<div key={product.id} className="product-card">
+					<a href={product.linkUrl} key={product.id} className="product-card">
 						{product.imageUrl && (
 							<div className="product-image">
 								<img src={product.imageUrl} alt={product.title} />
@@ -31,11 +31,10 @@ export default function save({ attributes }) {
 							className="product-description"
 						/>
 
-						<a href={product.linkUrl} className="product-link">
+						<div className="product-link button button__minimal">
 							{product.linkText}
-							<span className="arrow">→</span>
-						</a>
-					</div>
+						</div>
+					</a>
 				))}
 			</div>
 		</div>
